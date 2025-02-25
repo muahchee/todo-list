26c1d5070243da0c1c0bb53b444dc0c747b23278
muahchee <187490843+muahchee@users.noreply.github.com>
2025-02-25 19:14:58 +1300

    set up webpack

---
+++ webpack.dev.js
@@ -0,0 +1,33 @@
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+
+module.exports = merge(common, {
+
+  mode: 'development',
+
+  devtool: "inline-source-map",
+
+  devServer: {
+    watchFiles: ["./src/template.html"],
+    static: "./dist",
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+});
