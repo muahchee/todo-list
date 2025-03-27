79940efc32e8d651c40605fde1102056c26b08ae
muahchee <187490843+muahchee@users.noreply.github.com>
2025-03-28 00:04:55 +1300

    run code through prettier

--- src/scripts/taskStorage.js
+++ src/scripts/taskStorage.js
@@ -1,150 +1,126 @@
-
 //Local Storage will have the tasklistId as the key, and an array of objects(taskObjects) and the value
 //responsibility - manipulating the Local Storage
 
 export class TaskStorage {
+	constructor(taskListId, uniqueId) {
+		this.uniqueId = uniqueId;
 
-  constructor(taskListId, uniqueId) {
+		this.taskListId = taskListId;
+	}
 
-    this.uniqueId = uniqueId;
-    
-    this.taskListId = taskListId;
+	clearTaskList() {
+		localStorage.setItem(this.taskListId, JSON.stringify([]));
+	}
 
-  }
+	changeMainCheckboxState() {
+		const taskListArr = JSON.parse(localStorage.getItem(this.taskListId));
 
+		const currentTask = taskListArr.filter(
+			(task) => task["uniqueId"] === this.uniqueId,
+		)[0];
 
-  clearTaskList() {
+		const currentTaskIndex = taskListArr.indexOf(currentTask);
 
-    localStorage.setItem(this.taskListId, JSON.stringify([]))
+		if (currentTask["mainChecked"] === false) {
+			currentTask["mainChecked"] = true;
+		} else {
+			currentTask["mainChecked"] = false;
+		}
+		//replace task with updated one
+		taskListArr.splice(currentTaskIndex, 1, currentTask);
 
-  }
+		localStorage.setItem(this.taskListId, JSON.stringify(taskListArr));
 
-  changeMainCheckboxState() {
+		return currentTask["mainChecked"];
+	}
 
-    const taskListArr = JSON.parse(localStorage.getItem(this.taskListId));
+	changeInternalCheckboxState(key) {
+		const taskListArr = JSON.parse(localStorage.getItem(this.taskListId));
 
-    const currentTask = taskListArr.filter(task => task["uniqueId"] === this.uniqueId)[0];
+		const currentTask = taskListArr.filter(
+			(task) => task["uniqueId"] === this.uniqueId,
+		)[0];
 
-    const currentTaskIndex = taskListArr.indexOf(currentTask);
+		const currentTaskIndex = taskListArr.indexOf(currentTask);
 
-    if (currentTask["mainChecked"] === false){
+		if (currentTask[`checked${key}`] === false) {
+			currentTask[`checked${key}`] = true;
+		} else {
+			currentTask[`checked${key}`] = false;
+		}
 
-      currentTask["mainChecked"] = true;
+		//replace task with updated one
+		taskListArr.splice(currentTaskIndex, 1, currentTask);
 
-    } else {
+		localStorage.setItem(this.taskListId, JSON.stringify(taskListArr));
 
-      currentTask["mainChecked"] = false;
+		return currentTask[`checked${key}`];
+	}
 
-    };
-    //replace task with updated one 
-    taskListArr.splice(currentTaskIndex, 1, currentTask);
+	changeStoredTaskPriority() {
+		const taskListArr = JSON.parse(localStorage.getItem(this.taskListId));
 
-    localStorage.setItem(this.taskListId, JSON.stringify(taskListArr))
+		const currentTask = taskListArr.filter(
+			(task) => task["uniqueId"] === this.uniqueId,
+		)[0];
 
-    return currentTask["mainChecked"];
+		const currentTaskIndex = taskListArr.indexOf(currentTask);
 
-  }
+		if (currentTask["priority"] === false) {
+			currentTask["priority"] = true;
+		} else {
+			currentTask["priority"] = false;
+		}
+		//replace task with updated one
+		taskListArr.splice(currentTaskIndex, 1, currentTask);
 
-  changeInternalCheckboxState(key) {
+		localStorage.setItem(this.taskListId, JSON.stringify(taskListArr));
 
-    const taskListArr = JSON.parse(localStorage.getItem(this.taskListId));
+		return currentTask["priority"];
+	}
 
-    const currentTask = taskListArr.filter(task => task["uniqueId"] === this.uniqueId)[0];
+	storeTask(taskObject) {
+		this.taskObject = taskObject;
 
-    const currentTaskIndex = taskListArr.indexOf(currentTask);
+		//add a unique id property to each task, so I can target a particular one when editing and entry
+		this.taskObject["uniqueId"] = this.uniqueId;
 
-    if (currentTask[`checked${key}`] === false) {
+		const taskListArr = JSON.parse(localStorage.getItem(this.taskListId)) || [];
 
-      currentTask[`checked${key}`] = true;
+		taskListArr.push(this.taskObject);
 
-    } else {
+		localStorage.setItem(this.taskListId, JSON.stringify(taskListArr));
+	}
 
-      currentTask[`checked${key}`] = false;
+	editTask(taskObject) {
+		this.taskObject = taskObject;
 
-    }
+		const taskListArr = JSON.parse(localStorage.getItem(this.taskListId));
 
-    //replace task with updated one 
-    taskListArr.splice(currentTaskIndex, 1, currentTask);
+		const currentTask = taskListArr.filter(
+			(task) => task["uniqueId"] === this.uniqueId,
+		)[0];
 
-    localStorage.setItem(this.taskListId, JSON.stringify(taskListArr))
+		const currentTaskIndex = taskListArr.indexOf(currentTask);
 
-    return currentTask[`checked${key}`];
+		//replace task with updated one
+		taskListArr.splice(currentTaskIndex, 1, this.taskObject);
 
+		localStorage.setItem(this.taskListId, JSON.stringify(taskListArr));
+	}
 
-  }
+	deleteStoredTask() {
+		const taskListArr = JSON.parse(localStorage.getItem(this.taskListId));
 
-  changeStoredTaskPriority() {
+		const currentTask = taskListArr.filter(
+			(task) => task["uniqueId"] === this.uniqueId,
+		)[0];
 
-    const taskListArr = JSON.parse(localStorage.getItem(this.taskListId));
+		const currentTaskIndex = taskListArr.indexOf(currentTask);
 
-    const currentTask = taskListArr.filter(task => task["uniqueId"] === this.uniqueId)[0];
+		//remove task at index
+		taskListArr.splice(currentTaskIndex, 1);
 
-    const currentTaskIndex = taskListArr.indexOf(currentTask);
-
-    if (currentTask["priority"] === false){
-
-      currentTask["priority"] = true;
-
-    } else {
-
-      currentTask["priority"] = false;
-
-    };
-    //replace task with updated one 
-    taskListArr.splice(currentTaskIndex, 1, currentTask);
-
-    localStorage.setItem(this.taskListId, JSON.stringify(taskListArr))
-
-    return currentTask["priority"];
-
-  }
-
-  storeTask(taskObject) {
-
-    this.taskObject = taskObject
-
-    //add a unique id property to each task, so I can target a particular one when editing and entry
-    this.taskObject["uniqueId"] = this.uniqueId
-
-    const taskListArr = JSON.parse(localStorage.getItem(this.taskListId)) || []
-
-    taskListArr.push(this.taskObject)
-
-    localStorage.setItem(this.taskListId, JSON.stringify(taskListArr))
-
-  }
-
-  editTask(taskObject) {
-
-    this.taskObject = taskObject;
-
-    const taskListArr = JSON.parse(localStorage.getItem(this.taskListId));
-
-    const currentTask = taskListArr.filter(task => task["uniqueId"] === this.uniqueId)[0];
-
-    const currentTaskIndex = taskListArr.indexOf(currentTask);
-
-    //replace task with updated one 
-    taskListArr.splice(currentTaskIndex, 1, this.taskObject);
-
-    localStorage.setItem(this.taskListId, JSON.stringify(taskListArr))
-
-
-  }
-
-  deleteStoredTask() {
-
-    const taskListArr = JSON.parse(localStorage.getItem(this.taskListId));
-
-    const currentTask = taskListArr.filter(task => task["uniqueId"] === this.uniqueId)[0];
-
-    const currentTaskIndex = taskListArr.indexOf(currentTask);
-
-    //remove task at index
-    taskListArr.splice(currentTaskIndex, 1);
-
-    localStorage.setItem(this.taskListId, JSON.stringify(taskListArr))
-
-  }
-
+		localStorage.setItem(this.taskListId, JSON.stringify(taskListArr));
+	}
 }
